aa1b11b85641ac91c732d5a04f8e8d2dddaa344c
André Furquim <andreccomputacao@gmail.com>
2020-02-10 21:37:11 -0300

    Videos scoped by users

--- lib/rumbl_web/rumbl.ex
+++ lib/rumbl_web/rumbl.ex
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Programming Phoenix 1.4",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/phoenix14 for more book information.
+#---
 defmodule RumblWeb do
   @moduledoc """
   The entrypoint for defining your web interface, such
@@ -23,15 +31,15 @@ defmodule RumblWeb do
 
       import Plug.Conn
       import RumblWeb.Gettext
+      import RumblWeb.Auth, only: [authenticate_user: 2] # New import
       alias RumblWeb.Router.Helpers, as: Routes
     end
   end
 
   def view do
     quote do
-      use Phoenix.View,
-        root: "lib/rumbl_web/templates",
-        namespace: RumblWeb
+      use Phoenix.View, root: "lib/rumbl_web/templates",
+                        namespace: RumblWeb
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
@@ -50,6 +58,7 @@ defmodule RumblWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
+      import RumblWeb.Auth, only: [authenticate_user: 2] # New import
     end
   end
 
